20d8eee4ed27681aab152b960d6affd154a3dbc6
swatigakhar <55781428+swatigakhar@users.noreply.github.com>
2023-06-30 12:07:54 +0530

    updates

--- featureTest/login.feature
+++ featureTest/login.feature
@@ -2,9 +2,6 @@ Feature: Performing a login
 
   Scenario Outline: As a user, I can log into the given website
     Given I am on the login page
-    When I log in with a default user
+    When I log in with a default username and password
+    And I click the Submit button
     Then I shall be on the Success page
-  
-
-
-
--- stepDefinitionTest/login.stepdef.js
+++ stepDefinitionTest/login.stepdef.js
@@ -1,46 +1,19 @@
 //import {Given,When,Then} from 'cucumber';
 const { Given, When, Then } = require('@wdio/cucumber-framework');
 
-
-// Given(/^I am on the login page$/, function(){
-//     browser.url("https://practicetestautomation.com/practice-test-login/");
-// });
-
 Given(/^I am on the login page$/, async() => {
     await browser.url("https://practicetestautomation.com/practice-test-login/");
 });
 
-// When(/^I log in with a default user$/, function(){
-//     browser.$("//input[@name='username']").setValue("student");
-//     browser.$("//input[@name='password']").setValue("Password123");
-//     browser.$("//button[@id='submit']").click();
-// });
-
-When(/^I log in with a default user$/, async() => {
+When(/^I log in with a default username and password$/, async() => {
     await browser.$("//input[@name='username']").setValue("student");
     await browser.$("//input[@name='password']").setValue("Password123");
+});
+
+When(/^I click the Submit button$/, async() => {
     await browser.$("//button[@id='submit']").click();
 });
 
-// Then(/^I shall be on the Success page$/, function(){
-//     var strUrl =browser.getUrl();
-//     console.log("******URL is : " + strUrl);
-// });
-
 Then(/^I shall be on the Success page$/, async() => {
-    var strUrl =browser.getUrl();
-    console.log("******URL is : " + strUrl);
+    //await expect($("//h1[@class='post-title']")).toHaveTextContaining("Logged In Successfully");
 });
-
-// Given(/^I am on the login page$/, async() => {
-//     await browser.url("https://practicetestautomation.com/practice-test-login/");
-// });
-// When(/^I log in with a invalid user$/, async() => {
-//     await browser.$("//input[@name='username']").setValue("student");
-//     await browser.$("//input[@name='password']").setValue("dump");
-//     await browser.$("//button[@id='submit']").click();
-// });
-// Then(/^I shall get an error$/, async() => {
-//     var error = await browser.$("//input[@name='username']").setValue("student");
-//     await expect(error).toHaveTextContaining("Your username is invalid!");
-// });
